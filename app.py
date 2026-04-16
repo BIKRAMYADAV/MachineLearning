7e3acf66c151a7b80047da429fea3ba347461345
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2026-04-16 22:16:05 +0530

    feat: creates a fastapi server to utilize the models

--- app.py
+++ app.py
@@ -1,5 +1,36 @@
-import pandas as pd
+from fastapi import FastAPI
+import numpy as np
+import pickle 
 
-df = pd.read_csv("diabetes.csv")
+app = FastAPI()
 
-print(df.head())
+with open("model.pkl","rb") as f:
+    model = pickle.load(f)
+with open("scaler.pkl","rb") as f:
+    scaler = pickle.load(f)
+
+@app.get("/")
+def home():
+    return {"message": "health risk predictor for diabetes"}
+
+@app.post("/predict")
+def predict(data:dict):
+    features = np.array([
+          data["Pregnancies"],
+        data["Glucose"],
+        data["BloodPressure"],
+        data["SkinThickness"],
+        data["Insulin"],
+        data["BMI"],
+        data["DiabetesPedigreeFunction"],
+        data["Age"]
+    ]).reshape(1,-1)
+
+    features = scaler.transform(features)
+    prediction = model.predict(features)[0]
+    probability = model.predict_proba(features)[0][1]
+
+    return {
+        "prediction": int(prediction),
+        "risk_probabiltiy": float(probability)
+    }
